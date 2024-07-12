3e512680abfef6f12bda230a3fc675950147f754
unknown <bita.ftvt@gmail.com>
2024-07-12 03:48:35 -0400

    added some elements

--- src/components/ProjectCardModal/ProjectCardModal.tsx
+++ src/components/ProjectCardModal/ProjectCardModal.tsx
@@ -13,6 +13,7 @@ export default function ProjectCardModal(){
             placeholder="Enter your years of experience"
             type="text" 
             name="yearsofexperience" />
+            <button className="pcmodal__form--button">Apply Now</button>
         </form>
         </>
     )
