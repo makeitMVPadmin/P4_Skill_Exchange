28fc603407edf64ae844ed81efb707c54aea8310
emekathegreat <emekamba10@gmail.com>
2024-07-19 16:22:15 -0400

    added config files for eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,26 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "react/display-name": 0,
+    "react/no-unescaped-entities": "off",
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }
     ],
   },
 }
