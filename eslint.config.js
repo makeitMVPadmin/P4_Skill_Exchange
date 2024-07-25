c855d945ffd803ed0bef898aa5ffebadf48f2281
Michelle Lo <michellelo@live.ca>
2024-07-25 15:05:19 -0400

    fixed eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,34 @@
 import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from '@typescript-eslint'
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import reactConfig from 'eslint-plugin-react/configs/recommended.js'
 import { fixupConfigRules } from '@eslint/compat'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
+// Destructure the needed properties from the CommonJS module
+const { configs: tsConfigs } = tsPlugin
+
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
-  eslintConfigPrettier
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+      globals: globals.browser
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactConfig,
+      prettier: eslintConfigPrettier
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsConfigs.recommended.rules,
+      ...fixupConfigRules(reactConfig).rules,
+      ...eslintConfigPrettier.rules
+    }
+  }
 ]
