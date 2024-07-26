1a36855dd6477bbc6b4e7ea7b704d99160bbdf79
anaymous <max997123@gmail.com>
2024-07-26 16:13:04 -0400

    Remove test cases

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,7 @@ import SkillShare from "./pages/SkillSharePage/index.tsx";
 import Header from "./components/Header/Header.tsx";
 import Footer from "./components/Footer/Footer.tsx";
 
-// Test case
-/*
-import { editUserData } from "./utils/Firebase";
-import { getUserJobs } from "../src/utils/Firebase";
-const newUserData = {
-  email: "johnDoe@example.com",
-};
 
-editUserData("TAvb0pv0Ca5AjK2hoTDF", newUserData)
-console.log(getUserJobs("UID10035003"));
-*/
 function App() {
   return (
     <>
