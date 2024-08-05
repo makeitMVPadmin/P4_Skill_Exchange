fb4564100c064ee1f47026aef76d38544d9ba9fb
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-08-04 20:56:09 -0400

    added firebase data to marketplace page, updated the jobcard to get dynamic data and updated the category dropdown options and functionality

--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -1,59 +1,48 @@
 import { Link } from 'react-router-dom'
 
-type Job = {
-  id: number,
+interface Job {
+  id: string
+  usedId: string
+  categories: string[]
   title: string
+  header: string
+  about: string
   description: string
-  imageUrl: string
-  job_tags: string[]
+  thumbnail: string
+  assignedUser: string
+  status: number
+  createdAt: number
+  updatedAt: number
+  jobDuration: number
+  jobSkills: string[]
+  questions: number
 }
 
-type JobCardProps = {
+interface JobCardProps {
   job: Job
 }
 
 const JobCard = ({ job }: JobCardProps) => {
-
   return (
     <Link to={`/marketplace/${job.id}`}>
       <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-        <img
-          className="w-full"
-          src="https://via.placeholder.com/400x200"
-          alt="Sunset in the mountains"
-        />
+        <img className="w-full" src={job.thumbnail} alt={job.title} />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-            quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-            nihil.
-          </p>
+          <div className="font-bold text-xl mb-2">{job.title}</div>
+          <p className="text-gray-700 text-base">{job.description}</p>
         </div>
-        <button
-          className="border border-neutral-300 rounded-lg py-1.5 px-10 my-2 bg-blue-800 hover:bg-blue-600 text-white ml-6"
-        >
+        <button className="border border-neutral-300 rounded-lg py-1.5 px-10 my-2 bg-blue-800 hover:bg-blue-600 text-white ml-6">
           Apply
         </button>
         <div className="px-6 pt-4 pb-2">
-          {job.job_tags?.map((job_tag: string) => (
+          {job.categories?.map((category: string) => (
             <span
-              key={job_tag}
+              key={category}
               className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
             >
-              {job_tag}
+              {category}
             </span>
           ))}
-
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #winter
-          </span>
         </div>
       </div>
     </Link>
