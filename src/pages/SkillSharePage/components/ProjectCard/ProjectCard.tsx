4fe0fdd26da96ff7f393ba442b7c042867c70db4
Michelle Lo <michellelo@live.ca>
2024-08-11 11:07:00 -0400

    add status function to project card

--- src/pages/SkillSharePage/components/ProjectCard/ProjectCard.tsx
+++ src/pages/SkillSharePage/components/ProjectCard/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { setJobToInProgress } from '@/src/utils/Firebase'
 import './ProjectCard.scss'
 
 interface Project {
@@ -8,7 +10,7 @@ interface Project {
   thumbnail: string
   jobDuration: number
   categories: string[]
-  status: string
+  status: number
 }
 
 interface ProjectCardProps {
@@ -16,6 +18,47 @@ interface ProjectCardProps {
 }
 6
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
+  const [status, setStatus] = useState(project.status)
+
+  const handleStatusChange = async (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const newStatus = parseInt(event.target.value)
+    try {
+      if (newStatus === 1) {
+        await setJobToInProgress(project.id)
+      }
+      setStatus(newStatus)
+    } catch (error) {
+      console.error('Error updating status:', error)
+    }
+  }
+
+  //   const handleStatusClick = () => {
+  //     let newStatus
+  //     if (status === 0) {
+  //       newStatus = 1 // Move from Open to In Progress
+  //     } else if (status === 1) {
+  //       newStatus = 2 // Move from In Progress to Completed
+  //     } else {
+  //       newStatus = 0 // Reset to Open
+  //     }
+  //     updateStatus(newStatus)
+  //   }
+
+  const getStatusLabel = (status: number) => {
+    switch (status) {
+      case 0:
+        return 'Open'
+      case 1:
+        return 'In Progress'
+      case 2:
+        return 'Completed'
+      default:
+        return 'Unknown'
+    }
+  }
+
   return (
     <div className="project-card">
       <h3 className="project-card__title">{project.title}</h3>
@@ -43,9 +86,15 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
         </div>
         <div className="project-card__stage">
           <strong>PROJECT STAGE</strong>
-          <div className={`project-card__stage-status ${project.status}`}>
-            {project.status}
-          </div>
+          <select
+            value={status}
+            onChange={handleStatusChange}
+            className={`project-card__stage-status status-${status}`}
+          >
+            <option value={0}>Open</option>
+            <option value={1}>In Progress</option>
+            <option value={2}>Completed</option>
+          </select>
         </div>
       </div>
     </div>
